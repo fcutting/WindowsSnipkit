758a7eb03fe1a78df57e457182c8d7b067467f69
FletcherCutting <fletcher@fletchercutting.com.au>
2020-07-19 20:16:07 +1000

    [WSK-29] Update defer statements

--- Collection/Clipboard Data/Go/Collection-ClipboardData.go
+++ Collection/Clipboard Data/Go/Collection-ClipboardData.go
@@ -15,20 +15,20 @@ var (
 	dllKernel32      = syscall.NewLazyDLL("kernel32.dll")
 	procGlobalLock   = dllKernel32.NewProc("GlobalLock")
 	procGlobalUnlock = dllKernel32.NewProc("GlobalUnlock")
+	procCloseHandle  = dllKernel32.NewProc("CloseHandle")
 )
 
 func main() {
 	_, _, _ = procOpenClipboard.Call(0)
+	defer procCloseClipboard.Call()
 
 	handle, _, _ := procGetClipboardData.Call(13)
+	defer procCloseHandle.Call(handle)
 
 	lock, _, _ := procGlobalLock.Call(handle)
+	defer procGlobalUnlock.Call(handle)
 
 	text := syscall.UTF16ToString((*[1 << 20]uint16)(unsafe.Pointer(lock))[:])
-
-	_, _, _ = procGlobalUnlock.Call(handle)
-
-	procCloseClipboard.Call()
 
 	fmt.Println(text)
 }
--- Collection/Read File/Go/Collection-ReadFile.go
+++ Collection/Read File/Go/Collection-ReadFile.go
@@ -9,22 +9,24 @@ import (
 
 var (
 	dllKernel32       = syscall.NewLazyDLL("kernel32.dll")
-	funcCreateFileW   = dllKernel32.NewProc("CreateFileW")
-	funcReadFile      = dllKernel32.NewProc("ReadFile")
-	funcGetFileSize   = dllKernel32.NewProc("GetFileSize")
+	procCreateFileW   = dllKernel32.NewProc("CreateFileW")
+	procReadFile      = dllKernel32.NewProc("ReadFile")
+	procGetFileSize   = dllKernel32.NewProc("GetFileSize")
+	procCloseHandle   = dllKernel32.NewProc("CloseHandle")
 )
 
 func main() {
 	path, _ := syscall.UTF16PtrFromString(os.Args[1])
 
-	handle, _, _ := funcCreateFileW.Call(uintptr(unsafe.Pointer(path)), syscall.GENERIC_READ, 0, 0, syscall.OPEN_EXISTING, syscall.FILE_ATTRIBUTE_NORMAL, 0)
+	handle, _, _ := procCreateFileW.Call(uintptr(unsafe.Pointer(path)), syscall.GENERIC_READ, 0, 0, syscall.OPEN_EXISTING, syscall.FILE_ATTRIBUTE_NORMAL, 0)
+	defer procCloseHandle.Call(handle)
 
-	fileSize, _, _ := funcGetFileSize.Call(handle, 0)
+	fileSize, _, _ := procGetFileSize.Call(handle, 0)
 
 	buffer := make([]byte, fileSize)
 	var bytesRead *uint32
 
-	_, _, _ = funcReadFile.Call(handle, uintptr(unsafe.Pointer(&buffer[0])), uintptr(len(buffer)), uintptr(unsafe.Pointer(bytesRead)), 0)
+	_, _, _ = procReadFile.Call(handle, uintptr(unsafe.Pointer(&buffer[0])), uintptr(len(buffer)), uintptr(unsafe.Pointer(bytesRead)), 0)
 
 	fmt.Println(string(buffer))
 }
